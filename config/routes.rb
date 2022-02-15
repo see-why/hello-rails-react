162091a0c89eaad95aef16ef932d13af4f8d6b0a
see-why <iyadicy@gmail.com>
2022-02-15 18:57:01 +0100

    Add static view as app root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'static#index'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do 
      get 'random', to: 'greetings#random'
